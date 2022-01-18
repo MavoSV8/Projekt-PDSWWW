d2f58904ff5c99b2e04b99d264b83ecf57f51e12
Maciej Czajkowski <maciejpiotrczajkowski@gmail.com>
2022-01-18 22:10:44 +0100

    Add Webstorm project

---
+++ webstorm-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="app">
+  <Navbar/>
+  <div class="row border-5">
+    <LeftMenu class="vh-100 col-2 bg-white"/>
+    <Main class="vh-100 col-8 bg-light"/>
+    <RightMenu class="vh-100 col-2 bg-white"/>
+  </div>
+</div>
+</template>
+
+<script>
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "@/components/Navbar";
+import Main from "@/components/Main";
+import LeftMenu from "@/components/LeftMenu";
+import RightMenu from "@/components/RightMenu";
+
+export default {
+  name: 'App',
+  components: {
+    RightMenu,
+    LeftMenu,
+    Main,
+    Navbar
+    // Test
+  //   HelloWorld
+  }
+}
+
+
+</script>
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
---
+++ webstorm-project/src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+<div> Main </div>
+</template>
+
+<script>
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+  name: 'Main',
+  components: {
+    // Test
+  //   HelloWorld
+  }
+}
+
+
+</script>
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
