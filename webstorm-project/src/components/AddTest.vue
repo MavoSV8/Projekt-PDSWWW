06855d223ddb76c7621dbd8595234e8574504c48
MavosV8 <lukaszpasi@gmail.com>
2022-01-28 23:05:43 +0100

    added loading from external json

--- webstorm-project/src/components/AddTest.vue
+++ webstorm-project/src/components/AddTest.vue
@@ -2,45 +2,45 @@
   <div>
     <Header :title="'Nowy Test'"></Header>
     <div v-if="!submitted">
-    <div class="row d-flex text-dark justify-content-center">
-      <div class="col-10">
-        <div class="text-start bg-white mt-3 p-2 col-12 float-sm-start float-end" style="font-size: 15px">Czas
-          testu (minuty)
-          <input type="number" class="ms-3 float-end" placeholder="0"
-                 v-model="test.time">
+      <div class="row d-flex text-dark justify-content-center">
+        <div class="col-10">
+          <div class="text-start bg-white mt-3 p-2 col-12 float-sm-start float-end" style="font-size: 15px">Czas
+            testu (minuty)
+            <input type="number" class="ms-3 float-end" placeholder="0"
+                   v-model="test.time">
+          </div>
         </div>
-      </div>
-      <div class="col-10">
-        <div class="text-start bg-white mt-3 p-2 col-12 float-sm-start float-end" style="font-size: 15px">
-          Wczytaj z pliku JSON
-          <input type="file" class="ms-3 float-end" @change="loadJson">
+        <div class="col-10">
+          <div class="text-start bg-white mt-3 p-2 col-12 float-sm-start float-end" style="font-size: 15px">
+            Wczytaj z pliku JSON
+            <input type="file" class="ms-3 float-end" @change="loadJson">
+          </div>
         </div>
-      </div>
-      <div class="col-10">
-        <div class="text-start bg-white mt-3 p-2  col-12 float-sm-start float-end" style="font-size: 15px">Nazwa
-          testu
-          <input type="text" class="ms-3 float-end" placeholder="Wpisz nazwę testu..."
-                 v-model="test.title">
+        <div class="col-10">
+          <div class="text-start bg-white mt-3 p-2  col-12 float-sm-start float-end" style="font-size: 15px">Nazwa
+            testu
+            <input type="text" class="ms-3 float-end" placeholder="Wpisz nazwę testu..."
+                   v-model="test.title">
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <component
-            v-for="(question, index) in test.questions"
-            :key="index"
-            :is="question" :questionNum="index"
-            ref="questions"/>
-      </div>
-      <div class="col-10  float-sm-end">
-        <div class=" p-2 mt-3 float-sm-start">
-          <button type="button" class="btn btn-primary btn-sm" @click="addQuestion">Dodaj pytanie</button>
+        <div class="row">
+          <component
+              v-for="(question, index) in test.questions"
+              :key="index"
+              :is="question" :questionNum="index"
+              ref="questions"/>
         </div>
-        <div class=" p-2 mt-3 float-sm-start">
-          <button type="button" class="btn btn-primary btn-sm" @click="submitNewTest">Zapisz</button>
+        <div class="col-10  float-sm-end">
+          <div class=" p-2 mt-3 float-sm-start">
+            <button type="button" class="btn btn-primary btn-sm" @click="addQuestion">Dodaj pytanie</button>
+          </div>
+          <div class=" p-2 mt-3 float-sm-start">
+            <button type="button" class="btn btn-primary btn-sm" @click="submitNewTest">Zapisz</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
     <div v-if="submitted" class="row d-flex justify-content-center">Test zapisany!</div>
   </div>
 </template>
@@ -55,21 +55,44 @@ export default {
   name: "AddTest",
   data() {
     return {
-      submitted : false,
+      submitted: false,
       test: { // empty test, shall be filled with v-model
         id: 0,
         title: "",
         time: 0,
-        questions: [ NewQuestion ]
+        questions: [NewQuestion]
       },
     }
   },
   components: {NewQuestion, Header},
   methods: {
+
     loadJson(e) {
-      // TBD - not implemented
-      var files = e.target.files || e.dataTransfer.files;
-      console.log("AddTest: loadJson" + files[0]);
+      let files = e.target.files || e.dataTransfer.files;
+
+
+      const reader = new FileReader();
+
+      reader.addEventListener("load", () => {
+        console.log(reader.result);
+        let filesJson;
+        filesJson = JSON.parse(reader.result);
+        this.test.id = filesJson.id;
+        this.test.title = filesJson.title;
+        this.test.time = filesJson.time;
+        //this.test.questions = filesJson.questions;
+        filesJson.questions.forEach(entry => {
+          NewQuestion.data().msg = entry.msg;
+          NewQuestion.data().answers = entry.answers;
+        })
+        this.$forceUpdate();
+        //this.sendTest(this.test);
+        //console.log(this.id + ' ' + this.title + ' ' + this.time + ' ' + this.questions);
+      }, false);
+      reader.readAsText(files[0]);
+
+
+
     },
     addQuestion() {
       console.log("AddTest: adding question")
@@ -82,7 +105,7 @@ export default {
       console.log("AddTest: submiting new test")
       let outTest = {};
       // needs to be taken from db, first free
-      if ( this.test.title === "") {
+      if (this.test.title === "") {
         window.alert("Podaj nazwę testu!")
         return;
       }
@@ -93,7 +116,7 @@ export default {
       outTest.questions = [];
       for (const question of this.$refs['questions']) {
         let outQuestion = {};
-        if ( question.msg.length === 0 ) {
+        if (question.msg.length === 0) {
           window.alert("Występują puste pytania!")
           return;
         }
@@ -101,7 +124,7 @@ export default {
         outQuestion.answers = [];
         outQuestion.correct_answer = [];
         for (const answer of question.$refs['answers']) {
-          if ( answer.msg.length === 0 ) {
+          if (answer.msg.length === 0) {
             window.alert("Występują puste odpowiedzi dla któregoś z pytań!")
             return;
           }
@@ -109,7 +132,7 @@ export default {
           if (answer.correct) {
             outQuestion.correct_answer.push(answer.msg);
           }
-          if ( outQuestion.correct_answer.length === 0 ) {
+          if (outQuestion.correct_answer.length === 0) {
             window.alert("Brak poprawnych odpowiedzi dla któregoś z pytań! Jak tu zdać :)?")
             return;
           }
@@ -118,18 +141,22 @@ export default {
       }
       console.log(outTest);
 
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open("POST", "http://localhost:3000/tests",false);
-      xmlHttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-      xmlHttp.send(JSON.stringify(outTest));
-      console.log(JSON.stringify(outTest));
-      this.submitted = true
+      this.sendTest(outTest);
+      this.submitted = true;
     },
+    sendTest(testToSend){
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open("POST", "http://localhost:3000/tests", false);
+      xmlHttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      xmlHttp.send(JSON.stringify(testToSend));
+      console.log(JSON.stringify(testToSend));
+
+    }
   },
   mounted() {
     this.$root.$on('deleteQuestion', (index) => {
       console.log("AddTest: 'deleteQuestion' event received, " + index);
-      if ( this.test.questions.length > 1) {
+      if (this.test.questions.length > 1) {
         this.test.questions.splice(index, 1);
         // does not work correctly, I've got no idea whether is is fixable
       }
